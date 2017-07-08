c7bb0e9e8f428e3ac5d40970066dabbdde884a88
jason <jesusislord3@protonmail.com]>
2017-07-08 04:35:58 +0100

    Latest attempt at getting  the js files to load.

--- js/social.js
+++ js/social.js
@@ -1,10 +1,15 @@
-import {FB} from "//connect.facebook.net/en_GB/sdk.js";
-import * from "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js";
-import * as {HANDLEBARS} from "http://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-latest.js";
-import (facebook_settings,fbpage) from "app.js";
-;
 
+// // uncomment when mozzila has its way
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+// import {FB} from "//connect.facebook.net/en_GB/sdk.js";
+// import * from "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js";
+// import * as {HANDLEBARS} from "http://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-latest.js";
+// import (facebook_settings,fbpage) from "app.js";
 
+; // just to sort out dodgy script concats.
+
+$.getScript("app.js")
+$.getScript("https://cdn.jsdelivr.net/handlebarsjs/4.0.8/handlebars.min.js")
 
 var fbfeed = fbpage + '/feed';
 var template = Handlebars.compile( $("#social-tmpl").html() );

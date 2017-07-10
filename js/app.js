3d99ec66d1db0e941a2bd50c7a4e1f2580eddbac
Adam Crockett <mr.adam.crockett@gmail.com>
2017-07-10 14:12:49 +0100

    Clean up
    Your code is now loading inside of app.js
    
    You will find that the app may not be loading your data, this is likely a fb / my not serving it issue rather than a real issue.
    
    All your js is now not inline! keep it that way if posible.
    
    Loading how:
    I have polled for facebook before running anything so all your code will now load after the fact, jquery is safe to use now too. read the comments they explain everything.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,106 @@
-// javascript settings file.
+//for ease of use I have setup template from the Global window object, call template() instead of Handlebars.template or window.Handlebars.template (same thing)
+var template = window.Handlebars.template;
 
-var facebook_settings = {
+var fbpage = '156900194833233';
+var endpoints = {
+  //add your endpoints here as getters
+  get feed(){
+    return fbpage+"/feed";
+  }
+}
+var facebookSettings = {
     appId            : '139288266642775',
     autoLogAppEvents : true,
     status           : true,
     xfbml            : true,
     version          : 'v2.9' // or v2.8, v2.7, v2.6, v2.5, v2.4, v2.3,
+}
+
+
+// What to load when document object model is loaded.
+// 1 CREATE FB SCRIPT TAG DYNAMICLY TO START LOADING API - this allows FB init script to load EG.. initApp()
+window.addEventListener("DOMContentLoaded",function(){
+  
+  //I am patching without a server so to ensure it runs in real life I will get the origin and fix the js src origin acordingly.
+  var origin = location.origin === "file://" ? "https:":"";
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = origin+"//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   //now that the script tag is appended in the body lets run the callback bellow
+})
+//Init app is going to be attached to Global space and called instead of FBs code
+//We want to keep things in JS and not inline if posible.
+window.initApp = function(){
+  //FB is being an ass pain so lets poll for when it is defined!
+  var pollInterval = 100; //ms
+  var FbPoll = setInterval(function(){
+    var tries = 0;
+    if("FB" in window){
+      clearTimeout(FbPoll);
+      appOnInit();
+    }
+    else{
+      tries++
+      if(tries===10){
+        clearTimeout(FbPoll);
+        console.log("FACEBOOK cannot be loaded for some reason. please check your DOMContentLoaded for tracing.")
+      }
+    }
+  }, pollInterval)
+}
+
+
+var debug = true;
+
+/**
+ * THE MAIN BIT YOUR CODE GOES HERE
+ */
+function appOnInit(){  //document ready ;)
+  //DO STUFF HERE
+  FB.init(facebookSettings);
+  FB.AppEvents.logPageView();
+
+  //helpful hints
+  if(debug){ 
+    console.log("Running in debug mode, see useful info bellow.")
+    console.log("APP: ",JSON.stringify(Object.assign(
+      facebookSettings,
+      endpoints,
+      {facebookPage: fbpage}
+    ), null, "\t"))
   }
 
-var fbpage = '/156900194833233';
+  //CALLS
+  //returns an array of facebook "post" objects to "callback"
+  FB.api( endpoints.feed, fbsocialFeed );
+
+  //DEFINE HELPERS
+  function fbsocialFeed(callback) {
+
+
+    //TEMPLATING
+    $(callback).each(function (index, item) {
+      if("error" in item){
+        if(debug){console.log("FB Social has sent an error instead of a bunch of data! check your page id, app id, online status. what is stoping data?")}
+      }
+      else{
+        var fbSocialFeedContext = {
+          message: item['message'], 
+          picture_src: item['from']['picture'], 
+          name: item['from']['first_name'] 
+        }
+        if(debug){ console.log( template(fbSocialFeedContext) ) }
+
+        //template is defined at top of this script, see this for Handlebars details and how globals work.
+        $("#fbsocial").append( template(fbSocialFeedContext) );//render a template from the index.html <body>
+        }
+    });
+  }
+
+}

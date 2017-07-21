c591c5e6f5401cd63bfb435a5cc90a4fec2e03dc
adam <mr.adam.crockett@gmail.com>
2017-07-21 13:35:19 +0100

    debuging info for instragramfreed run

--- js/app.js
+++ js/app.js
@@ -147,5 +147,6 @@ function appOnInit(){  //document ready ;)
           clientId: '1c1c48d30d694bfa8244cc994ba4dae8',
       });
       // d.run();
-  instagramfeed.run();
+  var run = instagramfeed.run();
+  console.log(run)
 }

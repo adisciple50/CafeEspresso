3d7338e9bdefe2509d8cdbf61ac95395898f7e33
adam <mr.adam.crockett@gmail.com>
2017-07-21 13:48:42 +0100

    added instagramfeed error logging

--- js/app.js
+++ js/app.js
@@ -146,8 +146,12 @@ function appOnInit(){  //document ready ;)
           template : '<div class="col-md-3 container-fluid" id="image-{{id}}"><img src="{{picture}}" href="{{link}}"></div>',
           target : '#instagram-gallery',
           clientId: '1c1c48d30d694bfa8244cc994ba4dae8',
+          error(err){
+              console.log(err);
+          }
       });
       // d.run();
   var run = instagramfeed.run();
+
   console.log(run)
 }
